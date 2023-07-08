2a45a80408dec92a6c582f8c58e39061d3f5e388
Edwin Boon <edwin.boon@eo.nl>
2023-07-08 15:02:51 +0200

    Add login flow for spotify

--- src/routes/api/auth/callback/+server.ts
+++ src/routes/api/auth/callback/+server.ts
@@ -1,5 +1,42 @@
-import type { RequestHandler } from '@sveltejs/kit';
+import { error, redirect, type RequestHandler } from '@sveltejs/kit';
+import { CLIENT_ID, CLIENT_SECRET, BASE_URL } from '$env/static/private';
 
-export const GET: RequestHandler = (event) => {
-	console.log(event);
+export const GET: RequestHandler = async ({ url, cookies }) => {
+	const code = url.searchParams.get('code') || null;
+	const state = url.searchParams.get('state') || null;
+
+	const storedState = cookies.get('spotify_auth_state') || null;
+	const storedChallengeVerifier = cookies.get('spotify_auth_challenage_verifier') || null;
+
+	if (state === null || state !== storedState) {
+		throw error(403, 'unauthorized');
+	}
+	const response = await fetch('https://accounts.spotify.com/api/token', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			Authorization: `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+		},
+		body: new URLSearchParams({
+			code: code || '',
+			redirect_uri: `${BASE_URL}/api/auth/callback`,
+			grant_type: 'authorization_code',
+			code_verifier: storedChallengeVerifier || '',
+			client_id: CLIENT_ID
+		})
+	});
+
+	const responseJSON = await response.json();
+
+	if (responseJSON.error) {
+		throw error(400, responseJSON.error_description);
+	}
+
+	cookies.set('refresh_token', responseJSON.refresh_token, { path: '/' });
+	cookies.set('access_token', responseJSON.access_token, { path: '/' });
+
+	cookies.delete('spotify_auth_state');
+	cookies.delete('spotify_auth_challenage_verifier');
+
+	throw redirect(303, '/');
 };
--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -1,24 +1,26 @@
-import type { RequestHandler } from '@sveltejs/kit';
+import { redirect, type RequestHandler } from '@sveltejs/kit';
 import pkce from 'pkce-gen';
-import { redirect } from '@sveltejs/kit';
 import { CLIENT_ID, BASE_URL } from '$env/static/private';
 
-export const GET: RequestHandler = () => {
-	const generateRandomString = (length: number) => {
-		let randomString = '';
-		const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const generateRandomString = (length: number) => {
+	let randomString = '';
+	const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-		for (let i = 0; i < length; i++) {
-			randomString += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
-		}
-		return randomString;
-	};
+	for (let i = 0; i < length; i++) {
+		randomString += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+	}
+	return randomString;
+};
 
-	const state = generateRandomString(16);
-	const challenge = pkce.create();
+const state = generateRandomString(16);
+const challenge = pkce.create();
 
-	const scope =
-		'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read';
+const scope =
+	'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read';
+
+export const GET: RequestHandler = ({ cookies }) => {
+	cookies.set('spotify_auth_state', state);
+	cookies.set('spotify_auth_challenage_verifier', challenge.code_verifier);
 
 	throw redirect(
 		307,
